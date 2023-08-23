5db317a9ac5707dd31d5d55ed9f4b6eafde26cbb
Muhammad Royyan Zamzami <royyanmz87@gmail.com>
2023-08-23 11:28:55 +0700

    homepage

--- application/views/frontend/v_homepage.php
+++ application/views/frontend/v_homepage.php
@@ -13,9 +13,9 @@
   </div>
   <!--/ Intro Skew End /-->
 
-  <br/>
-  <br/>
-  <br/>
+  <br />
+  <br />
+  <br />
 
   <!--/ Section Services Star /-->
   <section id="service" class="services-mf route">
@@ -24,7 +24,7 @@
         <div class="col-sm-12">
           <div class="title-box text-center">
             <h3 class="title-a">
-              Visi dan Misi
+              Tentang Kami
             </h3>
             <p class="subtitle-a">
               SMP Negeri 22 Kota Tangerang Selatan
@@ -34,361 +34,20 @@
         </div>
       </div>
       <div class="row">
-        <div class="col-md-4">
-          <div class="service-box">
-            <div class="service-ico">
-              <span class="ico-circle"><i class="ion-stats-bars"></i></span>
-            </div>
-            <div class="service-content">
-              <h2 class="s-title">VISI</h2>
-              <p class="s-description text-center">
-                Unggul Dalam Prestasi dan kelembagaan, Berwawasan global, dilandasi Iman dan Taqwa.
-              </p>
-            </div>
-          </div>
+        <div class="col-md-6">
+          Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
+
+          The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.
         </div>
-        <div class="col-md-4">
-          <div class="service-box">
-            <div class="service-ico">
-              <span class="ico-circle"><i class="ion-connection-bars"></i></span>
-            </div>
-            <div class="service-content">
-              <h2 class="s-title">MISI</h2>
-              <p class="s-description text-center">
-                <p> 1. Mewujudkan lulusan SMP Negeri 22 Kota Tangerang Selatan yang unggul </p>
-                <p> 2. Mewujudkan kemampuan olah raga yang tangguh dan kompetitif </p>
-                <p> 3. Mewujudkan kemampuan di bidang seni yang tangguh dan kompetitif </p>
-                <p> 4. Meningkatkan kemampuan siswa, guru dan tenaga kependidikan dalam mengoptimalkan penggunaan media teknologi informasi dan komunikasi. </p>
-              </p>
-            </div>
-          </div>
-        </div>
-        <div class="col-md-4">
-          <div class="service-box">
-            <div class="service-ico">
-              <span class="ico-circle"><i class="ion-android-checkbox"></i></span>
-            </div>
-            <div class="service-content">
-              <h2 class="s-title">Tujuan</h2>
-              <p class="s-description text-center">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni adipisci eaque autem fugiat! Quia,
-                provident vitae! Magni
-                tempora perferendis eum non provident.
-              </p>
-            </div>
-          </div>
+        <div class="col-md-6">
+          Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
+
+          The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.
         </div>
       </div>
     </div>
   </section>
   <!--/ Section Services End /-->
-
-  <div class="section-counter paralax-mf bg-image" style="background-image: url(img/belajar-smp-bg.jpg)">
-    <div class="overlay-mf"></div>
-    <div class="container">
-      <div class="row">
-        <div class="col-sm-3 col-lg-3">
-          <div class="counter-box">
-            <div class="counter-ico">
-              <span class="ico-circle"><i class="ion-person-stalker"></i></span>
-            </div>
-            <div class="counter-num">
-              <p class="counter">60</p>
-              <span class="counter-text">JUMLAH SISWA</span>
-            </div>
-          </div>
-        </div>
-        <div class="col-sm-3 col-lg-3">
-          <div class="counter-box pt-4 pt-md-0">
-            <div class="counter-ico">
-              <span class="ico-circle"><i class="ion-university"></i></span>
-            </div>
-            <div class="counter-num">
-              <p class="counter">50</p>
-              <span class="counter-text">JUMLAH GURU DAN TENAGA KEPENDIDIKAN</span>
-            </div>
-          </div>
-        </div>
-        <div class="col-sm-3 col-lg-3">
-          <div class="counter-box pt-4 pt-md-0">
-            <div class="counter-ico">
-              <span class="ico-circle"><i class="ion-ios-people"></i></span>
-            </div>
-            <div class="counter-num">
-              <p class="counter">9</p>
-              <span class="counter-text">JUMLAH ROMBONGAN BELAJAR</span>
-            </div>
-          </div>
-        </div>
-        <div class="col-sm-3 col-lg-3">
-          <div class="counter-box pt-4 pt-md-0">
-            <div class="counter-ico">
-              <span class="ico-circle"><i class="ion-ios-home"></i></span>
-            </div>
-            <div class="counter-num">
-              <p class="counter">175</p>
-              <span class="counter-text">JUMLAH SARANA & PRASARANA</span>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-
-  <!--/ Section Portfolio Star /-->
-  <section id="work" class="portfolio-mf sect-pt4 route">
-    <div class="container">
-      <div class="row">
-        <div class="col-sm-12">
-          <div class="title-box text-center">
-            <h3 class="title-a">
-              FASILITAS DAN SARANA PENUNJANG LAINNYA
-            </h3>
-            <p class="subtitle-a">
-              SKH Assalam 02 Kota Tangerang Selatan
-            </p>
-            <div class="line-mf"></div>
-          </div>
-        </div>
-      </div>
-      <div class="row">
-        <div class="col-md-4">
-          <div class="work-box">
-            <a href="<?php echo base_url() ?>gambar/website/lab-komputer.jfif" data-lightbox="gallery-mf">
-              <div class="work-img">
-                <img src="<?php echo base_url(); ?>gambar/website/lab-komputer.jfif" alt="" class="img-fluid">
-              </div>
-              <div class="work-content">
-                <div class="row">
-                  <div class="col-sm-8">
-                    <h2 class="w-title">Lab. Komputer</h2>
-                    <div class="w-more">
-                      <span class="w-ctegory">Mohammad Arief Ramadhan</span> / <span class="w-date">29 Okt. 2020</span>
-                    </div>
-                  </div>
-                  <div class="col-sm-4">
-                    <div class="w-like">
-                      <span class="ion-ios-plus-outline"></span>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </a>
-          </div>
-        </div>
-        <div class="col-md-4">
-          <div class="work-box">
-            <a href="<?php echo base_url() ?>gambar/website/lapangan.jfif" data-lightbox="gallery-mf">
-              <div class="work-img">
-                <img src="<?php echo base_url(); ?>gambar/website/lapangan.jfif" alt="" class="img-fluid">
-              </div>
-              <div class="work-content">
-                <div class="row">
-                  <div class="col-sm-8">
-                    <h2 class="w-title">Lapangan Olahraga</h2>
-                    <div class="w-more">
-                      <span class="w-ctegory">Mohammad Arief Ramadhan</span> / <span class="w-date">29 Okt. 2020</span>
-                    </div>
-                  </div>
-                  <div class="col-sm-4">
-                    <div class="w-like">
-                      <span class="ion-ios-plus-outline"></span>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </a>
-          </div>
-        </div>
-        <div class="col-md-4">
-          <div class="work-box">
-            <a href="<?php echo base_url() ?>gambar/website/musholla.jpg" data-lightbox="gallery-mf">
-              <div class="work-img">
-                <img src="<?php echo base_url(); ?>gambar/website/musholla.jpg" alt="" class="img-fluid">
-              </div>
-              <div class="work-content">
-                <div class="row">
-                  <div class="col-sm-8">
-                    <h2 class="w-title">Musholla</h2>
-                    <div class="w-more">
-                      <span class="w-ctegory">Mohammad Arief Ramadhan</span> / <span class="w-date">29 Okt. 2020</span>
-                    </div>
-                  </div>
-                  <div class="col-sm-4">
-                    <div class="w-like">
-                      <span class="ion-ios-plus-outline"></span>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </a>
-          </div>
-        </div>
-        <div class="col-md-4">
-          <div class="work-box">
-            <a href="<?php echo base_url() ?>gambar/website/kantin.jfif" data-lightbox="gallery-mf">
-              <div class="work-img">
-                <img src="<?php echo base_url(); ?>gambar/website/kantin.jfif" alt="" class="img-fluid">
-              </div>
-              <div class="work-content">
-                <div class="row">
-                  <div class="col-sm-8">
-                    <h2 class="w-title">Kantin Siswa dan Guru</h2>
-                    <div class="w-more">
-                      <span class="w-ctegory">Mohammad Arief Ramadhan</span> / <span class="w-date">29 Okt. 2020</span>
-                    </div>
-                  </div>
-                  <div class="col-sm-4">
-                    <div class="w-like">
-                      <span class="ion-ios-plus-outline"></span>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </a>
-          </div>
-        </div>
-        <div class="col-md-4">
-          <div class="work-box">
-            <a href="<?php echo base_url() ?>gambar/website/perpustakaan.jpg" data-lightbox="gallery-mf">
-              <div class="work-img">
-                <img src="<?php echo base_url(); ?>gambar/website/perpustakaan.jpg" alt="" class="img-fluid">
-              </div>
-              <div class="work-content">
-                <div class="row">
-                  <div class="col-sm-8">
-                    <h2 class="w-title">Perpustakaan</h2>
-                    <div class="w-more">
-                      <span class="w-ctegory">Mohammad Arief Ramadhan</span> / <span class="w-date">29 Okt. 2020</span>
-                    </div>
-                  </div>
-                  <div class="col-sm-4">
-                    <div class="w-like">
-                      <span class="ion-ios-plus-outline"></span>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </a>
-          </div>
-        </div>
-        <div class="col-md-4">
-          <div class="work-box">
-            <a href="<?php echo base_url() ?>gambar/website/lab-ipa.jpg" data-lightbox="gallery-mf">
-              <div class="work-img">
-                <img src="<?php echo base_url(); ?>gambar/website/lab-ipa.jpg" alt="" class="img-fluid">
-              </div>
-              <div class="work-content">
-                <div class="row">
-                  <div class="col-sm-8">
-                    <h2 class="w-title">Lab.IPA</h2>
-                    <div class="w-more">
-                      <span class="w-ctegory">Mohammad Arief Ramadhan</span> / <span class="w-date">29 Okt. 2020</span>
-                    </div>
-                  </div>
-                  <div class="col-sm-4">
-                    <div class="w-like">
-                      <span class="ion-ios-plus-outline"></span>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </a>
-          </div>
-        </div>
-         <div class="col-md-4">
-          <div class="work-box">
-            <a href="<?php echo base_url() ?>gambar/website/uks.jfif" data-lightbox="gallery-mf">
-              <div class="work-img">
-                <img src="<?php echo base_url(); ?>gambar/website/uks.jfif" alt="" class="img-fluid">
-              </div>
-              <div class="work-content">
-                <div class="row">
-                  <div class="col-sm-8">
-                    <h2 class="w-title">Unit Kesehatan Sekolah</h2>
-                    <div class="w-more">
-                      <span class="w-ctegory">Mohammad Arief Ramadhan</span> / <span class="w-date">29 Okt. 2020</span>
-                    </div>
-                  </div>
-                  <div class="col-sm-4">
-                    <div class="w-like">
-                      <span class="ion-ios-plus-outline"></span>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </a>
-          </div>
-        </div>
-         <div class="col-md-4">
-          <div class="work-box">
-            <a href="<?php echo base_url() ?>gambar/website/aula.jpg" data-lightbox="gallery-mf">
-              <div class="work-img">
-                <img src="<?php echo base_url(); ?>gambar/website/aula.jpg" alt="" class="img-fluid">
-              </div>
-              <div class="work-content">
-                <div class="row">
-                  <div class="col-sm-8">
-                    <h2 class="w-title">Auditorium</h2>
-                    <div class="w-more">
-                      <span class="w-ctegory">Mohammad Arief Ramadhan</span> / <span class="w-date">29 Okt. 2020</span>
-                    </div>
-                  </div>
-                  <div class="col-sm-4">
-                    <div class="w-like">
-                      <span class="ion-ios-plus-outline"></span>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </a>
-          </div>
-        </div>
-        
-      </div>
-    </div>
-  </section>
-  <!--/ Section Portfolio End /-->
-
-  <!--/ Section Testimonials Star /-->
-  <div class="testimonials paralax-mf bg-image" style="background-image: url(assets_frontend/img/belajar-smp-bg.jpg)">
-    <div class="overlay-mf"></div>
-    <div class="container">
-      <div class="row">
-        <div class="col-md-12">
-          <div id="testimonial-mf" class="owl-carousel owl-theme">
-            <div class="testimonial-box">
-              <div class="author-test">
-                <img src="<?php echo base_url(); ?>gambar/website/profil.jpg" alt="" class="rounded-circle b-shadow-a">
-                <span class="author">Muhammad Rifki Permana</span>
-              </div>
-              <div class="content-test">
-                <p class="description lead">
-                  "Mengajar dan mengabdi di sekolah ini membuat saya lebih percaya akan pentingnya pendidikan karakter di masa kini dan yang akan datang,
-                  suasana sekolah yang berbeda dengan yang lain, di sini seluruh steckholder ikut berperan dalam peningkatan kualitas pendidikan karekter serta 
-                  terus berkomitmen dalam menciptakan generasi penerus bangsa yang berkualitas"
-                </p>
-                <span class="comit"><i class="fa fa-quote-right"></i></span>
-              </div>
-            </div>
-            <div class="testimonial-box">
-              <div class="author-test">
-                <img src="<?php echo base_url(); ?>gambar/website/profil2.jpg" alt="" class="rounded-circle b-shadow-a">
-                <span class="author">Muhammad Danutirta</span>
-              </div>
-              <div class="content-test">
-                <p class="description lead">
-                  "Pernah menjadi salah satu bagian dari sekolah ini adalah kebanggaan untuk ku, disini bukan hanya tempat untuk menimba ilmu saja, melainkan dapat 
-                  dijadikan rumah kedua bagi kita, dan di sini pun saya baru tersadar bahwa belajar itu ternayata se-asik ini"
-                </p>
-                <span class="comit"><i class="fa fa-quote-right"></i></span>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
 
   <!--/ Section Blog Star /-->
   <section id="blog" class="blog-mf sect-pt4 route">
@@ -408,12 +67,12 @@
       </div>
       <div class="row">
 
-        <?php foreach($artikel as $a){ ?>
+        <?php foreach ($artikel as $a) { ?>
           <div class="col-md-4">
             <div class="card card-blog">
               <div class="card-img">
-                <a href="<?php echo base_url().$a->artikel_slug ?>">
-                  <?php if($a->artikel_sampul != ""){ ?>
+                <a href="<?php echo base_url() . $a->artikel_slug ?>">
+                  <?php if ($a->artikel_sampul != "") { ?>
                     <img src="<?php echo base_url(); ?>gambar/artikel/<?php echo $a->artikel_sampul ?>" alt="" class="img-fluid">
                   <?php } ?>
                 </a>
@@ -425,7 +84,7 @@
                   </div>
                 </div>
 
-                <h3 class="card-title"><a href="<?php echo base_url().$a->artikel_slug ?>"><?php echo $a->artikel_judul ?></a></h3>
+                <h3 class="card-title"><a href="<?php echo base_url() . $a->artikel_slug ?>"><?php echo $a->artikel_judul ?></a></h3>
 
               </div>
               <div class="card-footer">
@@ -441,7 +100,7 @@
             </div>
           </div>
         <?php } ?>
-        
+
       </div>
     </div>
   </section>
